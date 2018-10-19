10f55dd99568935c4804f6c55ad42cb0140b9709
Yessenia García <al221611455@gmail.com>
2018-10-18 23:29:54 -0500

    primer commit

---
+++ database/migrations/2018_10_13_143315_practicas_de_ejecucion_de_competencias.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PracticasDeEjecucionDeCompetencias extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('practicasejecucioncom', function (Blueprint $table) {
+            $table->increments('id_com');
+			$table->string('periodo_in');
+            $table->string('periodo_fin');
+			$table->integer('horas_acum');
+			$table->integer('horas_tot');
+			$table->integer('num_periodo');
+			$table->integer('id_em')->unsigned();
+		    $table->foreign('id_em')->references('id_em')->on('empresas');
+			$table->integer('Matricula');
+		
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    
+    public function down()
+    {
+        Schema::drop('practicasejecucioncom');
+    }
+}
